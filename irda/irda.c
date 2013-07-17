17173f3488becbc082144af6d77a10e06cf40b4d
Ethan Chen <intervigil@gmail.com>
2013-07-16 23:48:52 -0700

    m7-common: Irda: basic enable, reset, send functions
    Change-Id: I71c566c54768c308018dfba77a4d4c7fde13a902

--- irda/irda.c
+++ irda/irda.c
@@ -15,9 +15,12 @@
  */
 
 #include <errno.h>
+#include <fcntl.h>
 #include <string.h>
-#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #define LOG_TAG "irda"
 #include <utils/Log.h>
@@ -25,12 +28,147 @@
 #include <hardware/hardware.h>
 #include <hardware/irda.h>
 
+#define CIR_DEVICE "/dev/ttyHSL2"
+#define CIR_RESET "/sys/class/htc_cir/cir/reset_cir"
+
+static int irda_enable(int on)
+{
+    int fd, ret = 0;
+    size_t bytes_written;
+    char enable;
+
+    /* Don't ask me why 0 == off, 1 == reset, 2 == on */
+    enable = on ? '2' : '0';
+
+    fd = open(CIR_RESET, O_WRONLY);
+    if (fd < 0) {
+        ALOGE("%s: Error opening %s: %d\n", __func__, CIR_RESET, errno);
+        ret = -EINVAL;
+        goto enable_err;
+    }
+
+    bytes_written = write(fd, &enable, 1);
+    if (bytes_written != 1) {
+        ALOGE("%s: Wrote %u bytes of %u!\n", __func__, bytes_written, 1);
+        ret = -ECOMM;
+        goto enable_err1;
+    }
+
+enable_err1:
+    close(fd);
+enable_err:
+    return ret;
+}
+
+static int irda_reset(void)
+{
+    int fd, ret;
+    size_t bytes_written;
+    char reset = '1';
+
+    /* Reset Irda device */
+    fd = open(CIR_RESET, O_WRONLY);
+    if (fd < 0) {
+        ALOGE("%s: Error opening %s: %d\n", __func__, CIR_RESET, errno);
+        ret = -EINVAL;
+        goto reset_err;
+    }
+
+    bytes_written = write(fd, &reset, 1);
+    if (bytes_written != 1) {
+        ALOGE("%s: Wrote %u bytes of %u!\n", __func__, bytes_written, 1);
+        ret = -ECOMM;
+        goto reset_err1;
+    }
+
+reset_err1:
+    close(fd);
+reset_err:
+    return ret;
+}
+
+static int irda_send(char *buf, size_t len)
+{
+    int fd, ret = 0;
+    size_t bytes_written;
+    struct termios t_opts;
+    char wakeup = -69;
+
+    /* TODO: are these necessary? */
+    ALOGI("%s: enabling\n", __func__);
+    irda_enable(1);
+    irda_reset();
+
+    fd = open(CIR_DEVICE, O_WRONLY | O_NOCTTY);
+    if (fd < 0) {
+        ALOGE("%s: Error opening %s: %d\n", __func__, CIR_DEVICE, errno);
+        ret = -EINVAL;
+        goto send_err;
+    }
+
+    ALOGI("%s: flushing\n", __func__);
+    tcflush(fd, TCIOFLUSH);
+
+    if (tcgetattr(fd, &t_opts) < 0) {
+        ALOGE("%s: Error getting device attributes: %d\n", __func__, errno);
+        ret = -EINVAL;
+        goto send_err1;
+    }
+
+    t_opts.c_iflag = IGNPAR;
+    t_opts.c_oflag = 0;
+    t_opts.c_cflag = t_opts.c_cflag & (0xFFFFE600 | 0x18F2);
+    t_opts.c_lflag = 0;
+    t_opts.c_line = 0;
+    //t_opts.c_cc = 0;
+
+    ALOGI("%s: setting attributes\n", __func__);
+    if (tcsetattr(fd, TCSANOW, &t_opts) < 0) {
+        ALOGE("%s: Error setting device attributes: %d\n", __func__, errno);
+        ret = -EINVAL;
+        goto send_err1;
+    }
+
+    ALOGI("%s: sending wakeup\n", __func__);
+    bytes_written = write(fd, &wakeup, 1);
+    if (bytes_written != 1) {
+        ALOGE("%s: Wrote %u bytes of %u\n", __func__, bytes_written, 1);
+        ret = -ECOMM;
+        goto send_err1;
+    }
+
+    ALOGI("%s: writing buffer\n", __func__);
+    bytes_written = write(fd, buf, len);
+    if (bytes_written != len) {
+        ALOGE("%s: Wrote %u bytes of %u!\n", __func__, bytes_written, len);
+        ret = -ECOMM;
+        goto send_err1;
+    }
+
+send_err1:
+    close(fd);
+send_err:
+    irda_enable(0);
+    return ret;
+}
+
 static void irda_send_ircode(char *buffer, int length)
 {
+    int i;
     char buf[length + 1];
+
     memcpy(buf, buffer, length);
-    buf[length] = 0;
+    buf[length] = 0; /* null terminate */
     ALOGI("Received IR buffer %s\n", buf);
+
+    /* HTC expects spaces it seems */
+    for (i = 0; i < length; i++) {
+        if (buffer[i] == ',') {
+            buffer[i] = ' ';
+        }
+    }
+
+    irda_send(buffer, length);
 }
 
 static int open_irda(const struct hw_module_t *module, const char *name,
